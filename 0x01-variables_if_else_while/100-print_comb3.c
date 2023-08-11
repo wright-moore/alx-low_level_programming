abc9910aca925484fea42a98bdf6ef86ea1e98c6
wright-moore <cartwrightkusi2002@gmail.com>
2023-08-11 16:13:20 -0700

    Betty revised 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int n, m;
 	
-        for (n = 48; n <= 56; n++)
+	for (n = 48; n <= 56; n++)
 	{
 		for (m = 49; m <= 57; m++)
 		{
@@ -25,5 +25,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return (0);
+        return (0);
 }
