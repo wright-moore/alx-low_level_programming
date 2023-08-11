42689aa02bdb062e8d4563d5ed5134308a708045
wright-moore <cartwrightkusi2002@gmail.com>
2023-08-11 14:45:44 -0700

    Betty revised 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 	int i, j;
-
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
@@ -25,7 +24,6 @@ int main(void)
 					putchar(' ');
 				}
 			}
-
 		}
 	}
 	putchar('\n');
